e0ffd22f9e63cf6ef6ec0a4e367a980a483ce8ce
Aniket Daripa <aniketdaripa84@gmail.com>
2023-07-09 22:00:05 +0530

    cors s

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-    'https://dripapp.netlify.app/',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
+    '*'
+    // 'https://dripapp.netlify.app/',
+    // 'http://127.0.0.1:5500',
+    // 'http://localhost:3500',
+    // 'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
